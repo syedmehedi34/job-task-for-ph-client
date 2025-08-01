bad0a47b5aa841687b3622ccdb9b0d21b2d25361
syedmehedi34 <syedmeehdi34@gmail.com>
2025-08-02 01:09:26 +0600

    favicon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,8 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Left: Logo */}
-          <div className="flex-shrink-0">
+          <div className="flex-shrink-0 flex items-center">
+            {/* <img src="./../../public/icon.png" alt="" className="w-7" /> */}
             <Link to="/" className="text-2xl font-bold">
               UniDesk
             </Link>
