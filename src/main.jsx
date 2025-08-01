8338bd5896cdaa49507eca42cb4d65e58976c1b5
syedmehedi34 <syedmeehdi34@gmail.com>
2025-08-01 16:18:09 +0600

    hero section added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/colors.css";
 import { router } from "./routes/routes.jsx";
 import { RouterProvider } from "react-router";
 import AuthProvider from "./providers/AuthProvider.jsx";
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 const Home = () => {
   return (
     <div>
-      <Header />
+      <Header onSearch={(query) => console.log(query)} />
     </div>
   );
 };
